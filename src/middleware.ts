899a4487077a84aa16dfeb9589963227243d9b53
Pierre LE BIHAN <lebihanp@hotmail.fr>
2025-05-17 20:59:47 +0200

    Update middleware.ts to conditionally redirect to default locale only if the path does not start with '/en/'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,18 @@
-import { redirectToDefaultLocale } from "astro:i18n";
+import { redirectToDefaultLocale, getLocaleByPath } from "astro:i18n";
 
 // function available with `manual` routing
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (ctx, next) => {
   if (ctx.url.pathname !== "/") {
-    const redirectResponse = redirectToDefaultLocale(ctx, 302);
-    if (!redirectResponse) {
-      return new Response("Redirect failed", { status: 500 });
+    // if does not start with /langs, redirect to default locale
+    if (!ctx.url.pathname.startsWith("/en/")) {
+      const redirectResponse = redirectToDefaultLocale(ctx, 302);
+      if (!redirectResponse) {
+        return new Response("Redirect failed", { status: 500 });
+      }
+      return redirectResponse;
     }
-    return redirectResponse;
   }
   return next();
 });
