55ceb132f8533a3c6258b36607adc51b9374be11
Pierre LE BIHAN <lebihanp@hotmail.fr>
2025-05-17 21:38:55 +0200

    Refactor middleware to streamline locale redirection logic and remove debug logging. Update LanguagePicker and 404 page to use current locale directly, enhancing localization handling.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,12 @@ import { redirectToDefaultLocale, pathHasLocale } from "astro:i18n";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (ctx, next) => {
-  console.log("🚀 ~ onRequest ~ ctx:", ctx);
-  console.log("🚀 ~ onRequest ~ ctx:", ctx.url);
-  if (ctx.url.pathname !== "/") {
-    if (!pathHasLocale(ctx.url.pathname)) {
-      const redirectResponse = redirectToDefaultLocale(ctx, 302);
-      if (!redirectResponse) {
-        return new Response("Redirect failed", { status: 500 });
-      }
-      return redirectResponse;
+  if (!pathHasLocale(ctx.url.pathname)) {
+    const redirectResponse = redirectToDefaultLocale(ctx, 302);
+    if (!redirectResponse) {
+      return new Response("Redirect failed", { status: 500 });
     }
+    return redirectResponse;
   }
   return next();
 });
