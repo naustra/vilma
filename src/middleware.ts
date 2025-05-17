5846d3a96f5cd2cb519294fd22678b95be05f021
Pierre LE BIHAN <lebihanp@hotmail.fr>
2025-05-17 20:55:52 +0200

    Refactor middleware.ts to import redirectToDefaultLocale correctly and maintain manual routing functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
+import { redirectToDefaultLocale } from "astro:i18n";
+
+// function available with `manual` routing
 import { defineMiddleware } from "astro:middleware";
-import { redirectToDefaultLocale } from "astro:i18n"; // function available with `manual` routing
 
 export const onRequest = defineMiddleware(async (ctx, next) => {
   if (ctx.url.pathname !== "/") {
