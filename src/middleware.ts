499496fcda6467ca4e97b0b7a8def1a8cabdf5e8
Pierre LE BIHAN <lebihanp@hotmail.fr>
2025-05-17 21:02:22 +0200

    Comment out middleware logic in middleware.ts to disable default locale redirection and manual routing functionality for future adjustments.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { redirectToDefaultLocale, getLocaleByPath } from "astro:i18n";
+// import { redirectToDefaultLocale, getLocaleByPath } from "astro:i18n";
 
-// function available with `manual` routing
-import { defineMiddleware } from "astro:middleware";
+// // function available with `manual` routing
+// import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware(async (ctx, next) => {
-  if (ctx.url.pathname !== "/") {
-    // if does not start with /langs, redirect to default locale
-    if (!ctx.url.pathname.startsWith("/en/")) {
-      const redirectResponse = redirectToDefaultLocale(ctx, 302);
-      if (!redirectResponse) {
-        return new Response("Redirect failed", { status: 500 });
-      }
-      return redirectResponse;
-    }
-  }
-  return next();
-});
+// export const onRequest = defineMiddleware(async (ctx, next) => {
+//   if (ctx.url.pathname !== "/") {
+//     // if does not start with /langs, redirect to default locale
+//     if (!ctx.url.pathname.startsWith("/en/")) {
+//       const redirectResponse = redirectToDefaultLocale(ctx, 302);
+//       if (!redirectResponse) {
+//         return new Response("Redirect failed", { status: 500 });
+//       }
+//       return redirectResponse;
+//     }
+//   }
+//   return next();
+// });
