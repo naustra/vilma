8ea146c338443fb25b7e1b2cb0becbd689a4710d
Pierre LE BIHAN <lebihanp@hotmail.fr>
2025-05-17 21:08:20 +0200

    Comment out the redirectToDefaultLocale option in astro.config.mjs to allow for future adjustments in localization handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     locales: ["en", "fr", "mk"],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      // redirectToDefaultLocale: true,
     },
   },
 });
